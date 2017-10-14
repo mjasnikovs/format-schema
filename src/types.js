6c9db55560b7a171a9e840706b504b54bbca818f
edgars <mjasnikovs@gmail.com>
2017-10-14 12:21:07 +0300

    Delete

---
+++ src/types.js
@@ -0,0 +1,11 @@
+const STRING = Symbol('STRING')
+const INT = Symbol('INT')
+const FLOAT = Symbol('FLOAT')
+const BOOLEAN = Symbol('BOOLEAN')
+
+module.exports = {
+	STRING,
+	INT,
+	FLOAT,
+	BOOLEAN
+}
