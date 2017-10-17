5873b5b8add9d9f6eab1c1e22458b59a7472e455
Edgars Mjasnikovs <EMJA@JWinc.jeld-wen.com>
2017-10-17 09:40:41 +0300

    String format added

--- src/formats/string.js
+++ src/formats/string.js
@@ -5,7 +5,9 @@ const {
 	isMaxString,
 	isMinString,
 	isEmail,
-	isString
+	isString,
+	isNaturalNumber,
+	isBoolean
 } = require('../validators')
 
 const {
@@ -39,9 +41,7 @@ const defaultConfig = {
 	email: false
 }
 
-module.exports = (value, options) => {
-	const config = Object.assign({}, defaultConfig, options)
-
+const stringFormat = (value, config) => {
 	if (config.notUndef === false) {
 		if (typeof value === 'undefined') {
 			return
@@ -125,4 +125,70 @@ module.exports = (value, options) => {
 	}
 
 	return string
+}
+
+module.exports = options => {
+	const config = Object.assign({}, defaultConfig, options)
+
+	if (!isString(config.name)) {
+		throw new Error(`Format configuration error. "name" param has invalid value "${config.name}". Expected string, found "${config.name}".`)
+	}
+
+	if (!isBoolean(config.trim)) {
+		throw new Error(`Format configuration error. "trim" param has invalid value "${config.trim}". Expected boolean, found "${config.trim}".`)
+	}
+
+	if (!isBoolean(config.trimLeft)) {
+		throw new Error(`Format configuration error. "trimLeft" param has invalid value "${config.trimLeft}". Expected boolean, found "${config.trimLeft}".`)
+	}
+
+	if (!isBoolean(config.trimRight)) {
+		throw new Error(`Format configuration error. "trimRight" param has invalid value "${config.trimRight}". Expected boolean, found "${config.trimRight}".`)
+	}
+
+	if (!isBoolean(config.toLowerCase)) {
+		throw new Error(`Format configuration error. "toLowerCase" param has invalid value "${config.toLowerCase}". Expected boolean, found "${config.toLowerCase}".`)
+	}
+
+	if (!isBoolean(config.toUpperCase)) {
+		throw new Error(`Format configuration error. "toUpperCase" param has invalid value "${config.toUpperCase}". Expected boolean, found "${config.toUpperCase}".`)
+	}
+
+	if (config.toLowerCase === true && config.toUpperCase === true) {
+		throw new Error('Format configuration error. "toUpperCase" and "toLowerCase" params can\'t be true at the same time.')
+	}
+
+	if (config.truncate !== false && !isNaturalNumber(config.truncate)) {
+		throw new Error(`Format configuration error. "truncate" param has invalid value "${config.truncate}". Expected false or natural number, found "${config.truncate}".`)
+	}
+
+	if (config.capitalize !== false && !inEnum(config.capitalize, ['words', 'sentences', 'first'])) {
+		throw new Error(`Format configuration error. "capitalize" param has invalid value "${config.capitalize}". Expected false, "words", "sentences" or "first", found "${config.capitalize}".`)
+	}
+
+	if (!isBoolean(config.notUndef)) {
+		throw new Error(`Format configuration error. "notUndef" param has invalid value "${config.notUndef}". Expected boolean, found "${config.notUndef}".`)
+	}
+
+	if (!isBoolean(config.notEmpty)) {
+		throw new Error(`Format configuration error. "notEmpty" param has invalid value "${config.notEmpty}". Expected boolean, found "${config.notEmpty}".`)
+	}
+
+	if (config.enum !== false && !Array.isArray(config.enum)) {
+		throw new Error(`Format configuration error. "enum" param has invalid value "${config.enum}". Expected false or array, found "${config.enum}".`)
+	}
+
+	if (config.min !== false && !isNaturalNumber(config.min)) {
+		throw new Error(`Format configuration error. "min" param has invalid value "${config.min}". Expected false or natural number, found "${config.min}".`)
+	}
+
+	if (config.max !== false && !isNaturalNumber(config.max)) {
+		throw new Error(`Format configuration error. "max" param has invalid value "${config.max}". Expected false or natural number, found "${config.max}".`)
+	}
+
+	if (!isBoolean(config.email)) {
+		throw new Error(`Format configuration error. "email" param has invalid value "${config.email}". Expected boolean, found "${config.email}".`)
+	}
+
+	return stringFormat
 }
