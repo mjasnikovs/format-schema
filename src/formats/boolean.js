6c35c6b0632d55d5eb2a5047452cce6aa39cbf42
Edgars Mjasnikovs <EMJA@JWinc.jeld-wen.com>
2017-10-18 10:14:28 +0300

    Boolean format added

---
+++ src/formats/boolean.js
@@ -0,0 +1,79 @@
+const {
+	notUndef,
+	notEmpty,
+	isString,
+	isBoolean,
+	isObject
+} = require('../validators')
+
+const defaultConfig = {
+	name: 'Boolean',
+
+	// validate
+	notUndef: false,
+	notEmpty: false
+}
+
+const booleanFormat = (value, config) => {
+	if (config.notUndef === false && config.notEmpty === false) {
+		if (typeof value === 'undefined') {
+			return
+		}
+	}
+
+	if (config.notUndef === true) {
+		if (!notUndef(value)) {
+			return new Error(`Format error. "${config.name}" has invalid value "${value}". Expected float, found undefined value.`)
+		}
+	}
+
+	if (config.notEmpty === false) {
+		if (value === null) {
+			return null
+		}
+	}
+
+	if (config.notEmpty === true) {
+		if (!notEmpty(value)) {
+			return new Error(`Format error. "${config.name}" has invalid value "${value}". Expected non-empty float, found "${value}".`)
+		}
+	}
+
+	if (!isBoolean(value)) {
+		return new Error(`Format error. "${config.name}" has invalid value "${value}". Expected boolean, found "${value}".`)
+	}
+
+	return value
+}
+
+module.exports = (options = null) => {
+	if (options === null) {
+		return value => booleanFormat(value, defaultConfig)
+	}
+
+	if (!isObject(options)) {
+		throw new Error(`Format configuration error. Configuration is invalid. Expected object, found "${options}".`)
+	}
+
+	const config = Object.assign({}, defaultConfig, options)
+
+	const invalidConfigKey = Object.keys(config).find(key => Object.keys(defaultConfig).indexOf(key) === -1)
+
+	if (typeof invalidConfigKey !== 'undefined') {
+		throw new Error(`Format configuration error. Configuration is invalid, param "${invalidConfigKey}" not found. Expected valid configuration object, found invalid key "${invalidConfigKey}".`)
+	}
+
+	if (!isString(config.name)) {
+		throw new Error(`Format configuration error. "name" param has invalid value "${config.name}". Expected string, found "${config.name}".`)
+	}
+
+	if (!isBoolean(config.notUndef)) {
+		throw new Error(`Format configuration error. "notUndef" param has invalid value "${config.notUndef}". Expected boolean, found "${config.notUndef}".`)
+	}
+
+	if (!isBoolean(config.notEmpty)) {
+		throw new Error(`Format configuration error. "notEmpty" param has invalid value "${config.notEmpty}". Expected boolean, found "${config.notEmpty}".`)
+	}
+
+	return value => booleanFormat(value, config)
+}
--- src/formats/index.js
+++ src/formats/index.js
@@ -1,9 +1,11 @@
 const stringFormat = require('./string')
 const integerFormat = require('./integer')
 const floatFormat = require('./float')
+const booleanFormat = require('./boolean')
 
 module.exports = {
 	stringFormat,
 	integerFormat,
-	floatFormat
+	floatFormat,
+	booleanFormat
 }
