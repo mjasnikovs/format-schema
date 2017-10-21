f16d8aa729daf1f3c251c500256cc8ffb4d2c52c
edgars <mjasnikovs@gmail.com>
2017-10-21 13:09:44 +0300

    Schema error reporting added

--- src/errorHandling.js
+++ src/errorHandling.js
@@ -1,3 +1,7 @@
+const {
+	isObject
+} = require('./validators')
+
 const flatenErrors = errors => {
 	if (Array.isArray(errors)) {
 		return errors
@@ -29,7 +33,35 @@ const formatErrors = errors => {
 	return null
 }
 
+const validateSchema = schema => {
+	if (!Array.isArray(schema) && !isObject(schema)) {
+		if (typeof schema !== 'function') {
+			return new Error(`Format schema error. Configuration is invalid. Expected functions, found "${schema}".`)
+		}
+
+		return null
+	}
+
+	if (Array.isArray(schema)) {
+		if (schema.length === 0) {
+			return new Error('Format schema error. Configuration is invalid. Expected non-empty array, found "[]".')
+		}
+		return schema.map(validateSchema)
+	}
+
+	if (isObject(schema)) {
+		if (Object.keys(schema).length === 0) {
+			return new Error('Format schema error. Configuration is invalid. Expected non-empty object, found "{}".')
+		}
+
+		return Object.keys(schema).map(key => validateSchema(schema[key]))
+	}
+
+	throw new Error(`Format schema error. Configuration is invalid. Found "${schema}".`)
+}
+
 module.exports = {
 	flatenErrors,
-	formatErrors
+	formatErrors,
+	validateSchema
 }
