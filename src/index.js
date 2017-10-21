ebc49859c8bbf5d33eb4b7c6bc865919cc3e423c
edgars <mjasnikovs@gmail.com>
2017-10-21 13:11:00 +0300

    Package entery index added

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const format = require('./format')
+
+const {
+	stringFormat,
+	integerFormat,
+	floatFormat,
+	booleanFormat
+} = require('./formats')
+
+module.exports = {
+	format,
+	stringFormat,
+	integerFormat,
+	floatFormat,
+	booleanFormat
+}
