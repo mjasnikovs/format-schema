45b363202c9f96156a5c1b644f09f96b66eacc4d
edgars <mjasnikovs@gmail.com>
2017-10-21 13:08:38 +0300

    Format default name update, to namespace

--- src/format.js
+++ src/format.js
@@ -1,14 +1,85 @@
 const {
-	isObject,
-	validateSchema
+	isObject
 } = require('./validators')
 
 const {
-	formatErrors
+	formatErrors,
+	validateSchema
 } = require('./errorHandling')
 
-const format = (schema, values) => {
-	return schema
+const {
+	NAMESPACE_DEFAULT_NAME
+} = require('./types')
+
+const format = (schema, values, namespace = '') => {
+	if (Array.isArray(schema)) {
+		if (schema.length === 1) {
+			if (Array.isArray(values)) {
+				let out = []
+				const valuesLength = values.length
+
+				for (let i = 0; i < valuesLength; i += 1) {
+					const localNamespace = namespace ? `[${i}]` : `${namespace}[${i}]`
+
+					const result = format(schema[0], values[i], localNamespace)
+
+					if (result instanceof Error) {
+						result.message = result.message.replace(NAMESPACE_DEFAULT_NAME, localNamespace)
+						return result
+					}
+
+					out = [...out, result]
+				}
+
+				return out
+			} else {
+				return new Error(`Format error. "${namespace}" has invalid value "${values}". Expected array, found "${values}".`)
+			}
+		} else if (Array.isArray(values)) {
+			let out = []
+			const schemaLength = schema.length
+
+			for (let i = 0; i < schemaLength; i += 1) {
+				const localNamespace = namespace ? `[${i}]` : `${namespace}[${i}]`
+
+				const result = format(schema[i], values[i], localNamespace)
+
+				if (result instanceof Error) {
+					result.message = result.message.replace(NAMESPACE_DEFAULT_NAME, localNamespace)
+					return result
+				}
+
+				out = [...out, result]
+			}
+
+			return out
+		}
+
+		return new Error(`Format error. "${namespace}" has invalid value "${values}". Expected array, found "${values}".`)
+	}
+
+	if (isObject(schema)) {
+		const schemaKeys = Object.keys(schema)
+
+		const out = {}
+		const schemaLength = schemaKeys.length
+
+		for (let i = 0; i < schemaLength; i += 1) {
+			const key = schemaKeys[i]
+			const localNamespace = namespace ? `${namespace}.${key}` : key
+
+			out[key] = format(schema[key], values[key], localNamespace)
+
+			if (out[key] instanceof Error) {
+				out[key].message = out[key].message.replace(NAMESPACE_DEFAULT_NAME, localNamespace)
+				return out[key]
+			}
+		}
+
+		return out
+	}
+
+	return schema(values)
 }
 
 module.exports = schema => {
@@ -16,10 +87,10 @@ module.exports = schema => {
 		throw new Error(`Format schema error. Configuration is invalid. Expected object or array, found "${schema}".`)
 	}
 
-	const schemaError = validateSchema(schema)
+	const schemaError = formatErrors(validateSchema(schema))
 
 	if (schemaError) {
-		throw formatErrors(schemaError)
+		throw schemaError
 	}
 
 	return value => format(schema, value)
--- src/types.js
+++ src/types.js
@@ -1,16 +1,10 @@
-const STRING = Symbol('STRING')
-const INT = Symbol('INT')
-const FLOAT = Symbol('FLOAT')
-const BOOLEAN = Symbol('BOOLEAN')
+const NAMESPACE_DEFAULT_NAME = 'NAMESPACE_DEFAULT_NAME'
 
 const MAX_INT = 2147483647
 const MIN_INT = -2147483648
 
 module.exports = {
-	STRING,
-	INT,
-	FLOAT,
-	BOOLEAN,
+	NAMESPACE_DEFAULT_NAME,
 	MAX_INT,
 	MIN_INT
 }
