f5e57ec326418351bac4f3afe34f9e5abda54f37
Edgars Mjasnikovs <mjasnikovs@gmail.com>
2019-03-18 08:12:26 +0200

    Remove JS files

--- src/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NAMESPACE_DEFAULT_NAME = 'NAMESPACE_DEFAULT_NAME';
-exports.MAX_INT = 2147483647;
-exports.MIN_INT = -2147483648;
